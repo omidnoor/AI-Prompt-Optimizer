c5b68688eb57987ad133c3cc150489b412cf60e8
Omid Noorshams <o.noorshams@gmail.com>
2024-01-11 20:32:19 -0800

    create prompt optimizer section

--- pages/upwork/index.js
+++ pages/upwork/index.js
@@ -6,43 +6,43 @@ import useStore from "store/store";
 import { controlSubmitFunction } from "util/openai/controlSubmitFunction";
 
 const upwork = () => {
-  const { thread, setThread, text, messagesList, setMessagesList } = useStore();
-  useAssistantInit();
+  // const { thread, setThread, text, messagesList, setMessagesList } = useStore();
+  // useAssistantInit();
 
-  useEffect(() => {
-    if (text.trim()) {
-      setMessagesList((prev) => [...prev, { role: "user", content: text }]);
-    }
-  }, [text, thread]);
+  // useEffect(() => {
+  //   if (text.trim()) {
+  //     setMessagesList((prev) => [...prev, { role: "user", content: text }]);
+  //   }
+  // }, [text, thread]);
 
-  const { handleMessageSequence } = useMessageSubmission(
-    thread,
-    setMessagesList
-  );
+  // const { handleMessageSequence } = useMessageSubmission(
+  //   thread,
+  //   setMessagesList
+  // );
 
-  const handleMessageSubmit = useCallback(
-    async (e) => {
-      e.preventDefault();
-      if (text.trim()) {
-        // let updatedThread = await addMessageToThread();
-        setMessagesList((prev) => [...prev, { role: "user", content: text }]);
-        let message = { role: "user", content: text };
-        handleMessageSequence(
-          message,
-          "assistant_upwork_job_analyzer",
-          controlSubmitFunction
-        );
-      }
-    },
-    [text, thread]
-  );
+  // const handleMessageSubmit = useCallback(
+  //   async (e) => {
+  //     e.preventDefault();
+  //     if (text.trim()) {
+  //       // let updatedThread = await addMessageToThread();
+  //       setMessagesList((prev) => [...prev, { role: "user", content: text }]);
+  //       let message = { role: "user", content: text };
+  //       handleMessageSequence(
+  //         message,
+  //         "assistant_upwork_job_analyzer",
+  //         controlSubmitFunction
+  //       );
+  //     }
+  //   },
+  //   [text, thread]
+  // );
   // console.log(messagesList);
   return (
     <>
-      <Upwork
+      {/* <Upwork
         messagesList={messagesList}
         handleMessageSubmit={handleMessageSubmit}
-      />
+      /> */}
     </>
   );
 };
